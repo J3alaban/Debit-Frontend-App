9a9233db7e9b68877e63356df264f4ad96cd2e10
DeLiF <j3alaban@gmail.com>
2026-06-23 13:50:57 +0300

    config ayarı değişti

--- src/helpers/Config.tsx
+++ src/helpers/Config.tsx
@@ -1,6 +1,6 @@
 export const Config ={
     api: {
         // Domain üzerinden gitmek Port 81 ve 8082 arasındaki CORS çakışmasını önler
-        baseUrl: "http://localhost:8080"
+        baseUrl: "http://62.72.21.81:3009"
     },
 }
